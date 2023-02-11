bc9b9fb98d2cba8f3b2cbc20221561654f4c4eb3
hakam00ra <epita73@gmail.com>
2023-02-11 16:16:15 +0200

    garbage router

--- garbage_router.py
+++ garbage_router.py
@@ -11,6 +11,7 @@ from PyQt5.QtWidgets import (
     QPushButton,
     QWidget,
 )
+import io
 import folium
 import requests
 import json
@@ -20,6 +21,7 @@ import geopy.distance
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from time import sleep
 
 
 
@@ -33,7 +35,30 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 class WorkerSignals(QObject):
-    result1 = pyqtSignal(str)   
+    mapReady = pyqtSignal(int)   
+    show = pyqtSignal(int) 
+
+
+class Window(QLabel):
+    def __init__(self):
+        super().__init__()
+        self.threadpool = QThreadPool()
+        self.setAlignment(Qt.AlignCenter)
+        self.map = QWebEngineView()
+        self.map.hide()
+
+
+    def showMap(self, maps):
+        self.map.show()
+        self.hide()
+        self.map.setHtml(open('map.html').read())
+
+    def showDrop(self):
+        self.show()
+        self.map.hide()
+        
+        
+        
 
 class App(QWidget):
     def __init__(self):
@@ -47,20 +72,28 @@ class App(QWidget):
         self.setLayout(mainLayout)
         self.ctrl = {'break': False}
         self.setAcceptDrops(True)
+        self.mapViewer = Window()
+        mainLayout.addWidget(self.mapViewer)
+        mainLayout.addWidget(self.mapViewer.map)
+        self.signals.mapReady.connect(self.mapViewer.showMap)
+        self.signals.show.connect(self.mapViewer.showDrop)
+        
     
     def dragEnterEvent(self, event):
+        self.signals.show.emit(1)
         if event.mimeData().hasText:
             event.accept()
         else:
             event.ignore()
 
-    def dragMoveEvent(self, event):
-        if event.mimeData().hasText:
+    def dragMoveEvent(self, event):   
+        self.signals.show.emit(1)     
+        if event.mimeData().hasText:            
             event.accept()
         else:
             event.ignore()
 
-    def dropEvent(self, event):       
+    def dropEvent(self, event):               
         event.setDropAction(Qt.CopyAction)    
         global fileDir        
         fileDir = event.mimeData().urls()[0].toLocalFile()        
@@ -73,13 +106,20 @@ class App(QWidget):
         coordinates = []
         for line in lines:
             lat, lon = line.split(', ')
-            coordinates.append((float(lat), float(lon)))            
+            coordinates.append((float(lat), float(lon)))   
+                     
         self.map_route(coordinates)
         
-    def map_route(coordinates):
+
+        
+
+    def load_map(self):
+        self.mapViewer.setUrl(QUrl.fromLocalFile(os.path.abspath("map.html")))
+
+    def map_route(self, coordinates):
         map = folium.Map(location=coordinates[0], zoom_start=13)
 
-        route = get_route(coordinates)
+        route = self.get_route(coordinates)
 
         folium.PolyLine(route, color="red", weight=2.5, opacity=1).add_to(map)
 
@@ -88,51 +128,35 @@ class App(QWidget):
 
         map.save("map.html")
         #webbrowser.open("map.html")
+        self.signals.mapReady.emit(1)
         print("Map saved as 'map.html'.")
-        view = QWebEngineView()
-        view.setHtml(map._repr_html_())
-        layout = QVBoxLayout()
-        layout.addWidget(view)
-        widget = QWidget()
-        widget.setLayout(layout)
-        widget.resize(800, 600)
-        widget.show()
+        return map 
+        
+
+    def get_route(self, coordinates):
+        coords = ";".join(f"{coord[1]},{coord[0]}" for coord in coordinates)
+        url = f"https://router.project-osrm.org/route/v1/driving/{coords}"
+        response = requests.get(url)
+        res = response.json()
+        poly = polyline.decode(res['routes'][0]['geometry'])
+        print(poly)
+        print(res['routes'][0]['distance'])
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            print("API response:", data)
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            return []
+
+        total_distance = 0
+        for i in range(len(poly) - 1):
+            coord1 = poly[i]
+            coord2 = poly[i + 1]
+            total_distance += geopy.distance.distance(coord1, coord2).km
+
+        return poly
 
 
-
-
-def get_route(coordinates):
-    coords = ";".join(f"{coord[1]},{coord[0]}" for coord in coordinates)
-    url = f"https://router.project-osrm.org/route/v1/driving/{coords}"
-    response = requests.get(url)
-    res = response.json()
-    rrr = polyline.decode(res['routes'][0]['geometry'])
-    print(polyline.decode(res['routes'][0]['geometry']))
-    print(res['routes'][0]['distance'])
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        print("API response:", data)
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-        return []
-
-    # Extract the route coordinates
-    #route = [[point[1], point[0]] for point in data["routes"][0]["geometry"]["coordinates"]]
-    total_distance = 0
-    for i in range(len(rrr) - 1):
-        coord1 = rrr[i]
-        coord2 = rrr[i + 1]
-        total_distance += geopy.distance.distance(coord1, coord2).km
-
-    #print("Total Distance:", total_distance, "km")
-    return rrr
-
-"""
-class Window(QLabel):
-    def __init__(self):
-        super().__init__()
-        self.threadpool = QThreadPool()
-"""
 
 
 application = QApplication(sys.argv)
